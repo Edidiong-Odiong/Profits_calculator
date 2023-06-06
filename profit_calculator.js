b6d8a45edab612f7884f958268a0da5c29a3c2aa
OdiongEdidiong <odiongedidiong79@gmail.com>
2023-06-06 09:10:59 +0100

    A site that calculates profits on investments

---
+++ profit_calculator.js
@@ -0,0 +1,12 @@
+function gain(a,b,c){
+    var capital = parseInt(a);
+    var duration = parseInt(b);
+    var rate = parseInt(c);
+    var cycle=0;
+    while(cycle<duration){
+        var profit= capital +((rate/100) * capital);
+        var capital =profit;
+        var cycle = cycle + 1;
+    };
+    document.write(`<h2 id="show">After ${duration} days, you will get ${capital}</h2><br><button id="refresh" onclick="window.location.reload()">Calculate again</button>`);
+}
