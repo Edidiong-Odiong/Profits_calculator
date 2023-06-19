499ffb61a8d877239471a041a9822912c7e7a9eb
OdiongEdidiong <odiongedidiong0@gmail.com>
2023-06-19 17:09:06 +0100

    Better design, plus now calculates float values

--- profit_calculator.js
+++ profit_calculator.js
@@ -1,13 +1,14 @@
 function gain(a,b,c){
-    var capital = parseInt(a);
-    var duration = parseInt(b);
-    var rate = parseInt(c);
+    var capital = parseFloat(a);
+    var duration = parseFloat(b);
+    var rate = parseFloat(c);
     var cycle=0;
     while(cycle<duration){
         var profit= capital +((rate/100) * capital);
         var capital =profit;
         var cycle = cycle + 1;
     };
-    document.getElementById("show").innerHTML = `<h2 id="show">After ${duration} years, you will get $${capital}</h2><br><button id="refresh" onclick="window.location.reload()">Calculate again</button>`;
+    var capital = capital.toFixed(10)
+    document.getElementById("show").innerHTML = `<h2 class="screen" id="show">After ${duration} years, you will get $${capital}</h2><br><button class="show_button" id="refresh" onclick="window.location.reload()">Calculate again</button>`;
 }
 // hmm
